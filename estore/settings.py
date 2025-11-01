0a35599b3415768c26ffdeba37f86c81dd5d262a
DRAMTECH <90094120+andreletou@users.noreply.github.com>
2025-11-01 02:50:56 +0000

    Update settings.py

--- estore/settings.py
+++ estore/settings.py
@@ -93,36 +93,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'estore.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-import dj_database_url
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-if os.environ.get('RENDER'):
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
-            ssl_require=True
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # chemin absolu
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
-
-
-
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
